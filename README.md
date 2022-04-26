5d09e8fd7a5542023a75c384dc2405f50050a483
Keenan Turley <keenan.turley@gmail.com>
2022-04-25 22:28:34 -0700

    Render square using indices, log when rendering

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ C++ Graphics Sandbox
 
 # Status
 Learning OpenGL in C++ with [TheCherno's Youtube series](https://www.youtube.com/playlist?list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2).
-Currently on [Index Buffers in OpenGL](https://www.youtube.com/watch?v=MXNMC1YAxVQ&list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2&index=9)
+Next video:[Dealing with Errors in OpenGL](https://www.youtube.com/watch?v=FBbPWSOQ0-w&list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2&index=10)
--- lizual/src/main.cpp
+++ lizual/src/main.cpp
@@ -153,19 +153,30 @@ int main(void)
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
     // Create vertex buffer 
-    float positions[6] = {
+    float positions[] = {
         -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f
+         0.5f, -0.5f,
+         0.5f,  0.5f,
+        -0.5f,  0.5f,
     };
 
-    GLuint buffer;
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * 2 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
     
     ShaderProgramSource sources = ParseShader("res/shaders/Basic.shader");
 
@@ -174,12 +185,13 @@ int main(void)
 
 
     /* Loop until the user closes the window */
+    std::cout << "Rendering...\n";
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
