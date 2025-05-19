19e93e5de45eecdb9cb237f36ab79c783273dc12
keenanturley <keenan.turley@gmail.com>
2025-05-18 20:35:14 -0700

    Add SDL3

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_init.h>
+
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+    std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+  std::cout << "SDL Initialized successfully!" << std::endl;
   return 0;
 }
