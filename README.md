ba344e798663e5508af0a01cea9d7360ae4adadd
keenanturley <keenan.turley@gmail.com>
2025-05-18 21:05:29 -0700

    Create SDL window

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # lizual
 C++ Graphics Sandbox
 
-# Installation & Running
+## Installation & Running
 ```sh
+# Clone the repo and the submodules
+git clone --recurse-submodules https://github.com/keenanturley/lizual.git
+cd lizual
+
+# Initialize CMake where -S is the source folder and -B is the build folder
 cmake -S . -B build
+
+# Build the executable
 cmake --build build
 
-# Windows
+# [Windows] Run the app
 build/Debug/lizual.exe
 ```
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,43 @@
-#include <iostream>
-
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_init.h>
+#include <SDL3/SDL_log.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_render.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+  // Initialize SDL
   if (!SDL_Init(SDL_INIT_VIDEO)) {
-    std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+    SDL_LogCritical(SDL_LOG_CATEGORY_ERROR, "SDL_Init failed: %s",
+                    SDL_GetError());
+    SDL_Quit();
     return 1;
   }
-  std::cout << "SDL Initialized successfully!" << std::endl;
+  SDL_Log("SDL initialized successfully");
+
+  SDL_Window *window = nullptr;
+  SDL_Renderer *renderer = nullptr;
+  if (!SDL_CreateWindowAndRenderer("Lizual", 640, 480, 0, &window, &renderer)) {
+    SDL_LogCritical(SDL_LOG_CATEGORY_ERROR,
+                    "SDL_CreateWindowAndRenderer failed: %s", SDL_GetError());
+    SDL_Quit();
+    return 1;
+  }
+  SDL_Log("Window and renderer created successfully");
+
+  SDL_Event event;
+  while (1) {
+    SDL_PollEvent(&event);
+    if (event.type == SDL_EVENT_QUIT) {
+      break;
+    }
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_Log("Exiting...");
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+
   return 0;
 }
